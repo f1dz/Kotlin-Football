29a656aed12b4045f5b557b7796c07c9c81e4da7
Khofidin <offiedz@gmail.com>
2018-10-16 23:35:43 +0700

    Clean code

--- app/src/main/java/in/khofid/dicoding/MainActivity.kt
+++ app/src/main/java/in/khofid/dicoding/MainActivity.kt
@@ -3,7 +3,6 @@ package `in`.khofid.dicoding
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
