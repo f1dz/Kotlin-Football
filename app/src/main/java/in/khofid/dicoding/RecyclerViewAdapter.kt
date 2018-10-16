7286792abd713157967edab017c6d14a032dcf91
Khofidin <offiedz@gmail.com>
2018-10-16 20:27:57 +0700

    Refactor using KTX

--- app/src/main/java/in/khofid/dicoding/RecyclerViewAdapter.kt
+++ app/src/main/java/in/khofid/dicoding/RecyclerViewAdapter.kt
@@ -5,9 +5,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.squareup.picasso.Picasso
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_list.*
+import kotlinx.android.synthetic.main.item_list.view.*
 
 class RecyclerViewAdapter(private val context: Context, private val items: List<Item>, private val listener: (Item) -> Unit)
     : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(){
@@ -21,15 +23,13 @@ class RecyclerViewAdapter(private val context: Context, private val items: List<
 
     override fun getItemCount(): Int = items.size
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
-
-        private val name = view.findViewById<TextView>(R.id.name)
-        private val image = view.findViewById<ImageView>(R.id.image)
+    class ViewHolder(override val containerView: View): RecyclerView.ViewHolder(containerView), LayoutContainer{
 
         fun bindItem(items: Item, listener: (Item) -> Unit){
-            name.text = items.name
+            itemView.name.text = items.name
+            Glide.with(containerView).load(items.image).into(image)
             items.image?.let { Picasso.get().load(it).into(image) }
-            itemView.setOnClickListener { listener(items) }
+            containerView.setOnClickListener { listener(items) }
         }
     }
 }
