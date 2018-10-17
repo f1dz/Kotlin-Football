314ef9b8cb3e1934a3ebeff84af0043003622cda
Khofidin <offiedz@gmail.com>
2018-10-17 15:27:13 +0700

    Change xml layout to anko

--- app/src/main/java/in/khofid/dicoding/MainActivity.kt
+++ app/src/main/java/in/khofid/dicoding/MainActivity.kt
@@ -1,24 +1,66 @@
 package `in`.khofid.dicoding
 
+import `in`.khofid.dicoding.R.color.colorAccent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.Spinner
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.*
+import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
     private var items: MutableList<Item> = mutableListOf()
+    private lateinit var listTeam: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        initUI()
         initData()
 
-        club_list.layoutManager = LinearLayoutManager(this)
-        club_list.adapter = RecyclerViewAdapter(this, items){
+        listTeam.adapter = RecyclerViewAdapter(this, items){
             startActivity<ClubDetail>("club" to it)
+        }
+    }
+
+    fun initUI() {
+        linearLayout {
+            lparams(width = matchParent, height = wrapContent)
+            orientation = LinearLayout.VERTICAL
+            topPadding = dip(16)
+            leftPadding = dip(16)
+            rightPadding = dip(16)
+
+            spinner = spinner()
+            swipeRefresh = swipeRefreshLayout{
+                setColorSchemeResources(colorAccent,
+                        android.R.color.holo_green_light,
+                        android.R.color.holo_orange_light,
+                        android.R.color.holo_red_light)
+
+                relativeLayout {
+                    lparams(width = matchParent, height = wrapContent)
+
+                    listTeam = recyclerView {
+                        lparams(width = matchParent, height = wrapContent)
+                        layoutManager = LinearLayoutManager(ctx)
+                    }
+
+                    progressBar = progressBar{}.lparams {
+                        centerHorizontally()
+                    }
+                }
+            }
         }
     }
 
