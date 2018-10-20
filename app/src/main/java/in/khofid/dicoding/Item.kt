297564b47e74b1039d23d0a733654f4b3840be5f
Khofidin <offiedz@gmail.com>
2018-10-20 15:28:13 +0700

    Delete unused files

--- app/src/main/java/in/khofid/dicoding/Item.kt
+++
@@ -1,7 +0,0 @@
-package `in`.khofid.dicoding
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class Item (val name: String?, val image: Int?, val detail: String?): Parcelable
