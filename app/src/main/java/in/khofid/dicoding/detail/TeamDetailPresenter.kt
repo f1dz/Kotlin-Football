4e5be303df8ebd27219b7e4d5b72609b7068128d
Khofidin <offiedz@gmail.com>
2018-10-23 10:16:34 +0700

    Add Unit Test

--- app/src/main/java/in/khofid/dicoding/detail/TeamDetailPresenter.kt
+++ app/src/main/java/in/khofid/dicoding/detail/TeamDetailPresenter.kt
@@ -4,6 +4,9 @@ import `in`.khofid.dicoding.api.ApiRepository
 import `in`.khofid.dicoding.api.TheSportDBApi
 import `in`.khofid.dicoding.model.TeamResponse
 import com.google.gson.Gson
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import org.jetbrains.anko.coroutines.experimental.bg
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -13,16 +16,15 @@ class TeamDetailPresenter(private val view: TeamDetailView,
 
     fun getTeamDetail(teamId: String) {
         view.showLoading()
-        doAsync{
-            val data = gson.fromJson(apiRepository
-                    .doRequest(TheSportDBApi.getTeamDetail(teamId)),
-                    TeamResponse::class.java
-            )
-
-            uiThread {
-                view.hideLoading()
-                view.showTeamDetail(data.teams)
+        async(UI) {
+            val data = bg {
+                gson.fromJson(apiRepository
+                        .doRequest(TheSportDBApi.getTeamDetail(teamId)),
+                        TeamResponse::class.java
+                )
             }
+            view.showTeamDetail(data.await().teams)
+            view.hideLoading()
         }
     }
 }
--- app/src/main/java/in/khofid/dicoding/teams/TeamsPresenter.kt
+++ app/src/main/java/in/khofid/dicoding/teams/TeamsPresenter.kt
@@ -3,25 +3,27 @@ package `in`.khofid.dicoding.teams
 import `in`.khofid.dicoding.api.ApiRepository
 import `in`.khofid.dicoding.api.TheSportDBApi
 import `in`.khofid.dicoding.model.TeamResponse
+import `in`.khofid.dicoding.utils.CoroutineContextProvider
 import com.google.gson.Gson
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
+import kotlinx.coroutines.experimental.async
+import org.jetbrains.anko.coroutines.experimental.bg
 
 class TeamsPresenter(private val view: TeamView,
                      private val apiRepository: ApiRepository,
-                     private val gson: Gson) {
+                     private val gson: Gson,
+                     private val context: CoroutineContextProvider = CoroutineContextProvider()) {
 
     fun getTeamList(league: String?){
         view.showLoading()
-        doAsync {
-            val data = gson.fromJson(
-                    apiRepository.doRequest(TheSportDBApi.getTeams(league)),
-                    TeamResponse::class.java
-            )
-            uiThread {
-                view.hideLoading()
-                view.showTeamList(data.teams)
+        async(context.main) {
+            val data = bg {
+                gson.fromJson(
+                        apiRepository.doRequest(TheSportDBApi.getTeams(league)),
+                        TeamResponse::class.java
+                )
             }
+                view.showTeamList(data.await().teams)
+                view.hideLoading()
         }
     }
 }
